3e36f4d45dc4f3db2d6b0a088a0647446238e931
malathidevi <132807099+malathidevi@users.noreply.github.com>
2023-07-23 12:47:40 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template, request
+from itertools import zip_longest
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/calculate', methods=['POST'])
+def calculate():
+    subjects = request.form.getlist('subject')
+    marks = request.form.getlist('mark')
+    
+    try:
+
+        marks = [int(mark) for mark in marks]
+    except ValueError:
+        return "Invalid input: marks must be numeric."
+    
+    avg_mark = sum(marks) / len(marks)
+    return render_template('result.html', subjects=subjects, marks=marks, avg_mark=avg_mark, zip=zip_longest)
+
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
